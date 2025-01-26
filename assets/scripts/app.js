2f0d1181add914b27709cfa4ab1ac13e0b23a1ba
Joao Victor Brol Carneiro <joaovbrolcarneiro@Joao-Carneiros-MacBook-Air.local>
2025-01-26 18:23:12 +0000

    log entries

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,5 +1,6 @@
 const defaultResult = 0;
 let currentResult = defaultResult;
+let logEntries = [];
 
 function getUserInput()
 {
@@ -9,22 +10,41 @@ function getUserInput()
 function add()
 {
     const enteredNumber = getUserInput();
+    const secondNumber = currentResult;
     const calcDescription = `${currentResult} + ${enteredNumber}`;
     currentResult = currentResult + enteredNumber;
     outputResult(currentResult, calcDescription);
+    const logEntry = {
+        operation: 'ADD',
+        previousResult: secondNumber,
+        newNumber: enteredNumber,
+        result : currentResult,
+    }
+    logEntries.push(logEntry);
+    console.log(logEntries);
 }
 
 function subtract()
 {
     const enteredNumber = getUserInput();
+    const secondNumber = currentResult;
     const calcDescription = `${currentResult} - ${enteredNumber}`;
     currentResult = currentResult - enteredNumber;
     outputResult(currentResult, calcDescription);
+    const logEntry = {
+        operation: 'SUBTRACT',
+        previousResult: secondNumber,
+        newNumber: enteredNumber,
+        result : currentResult,
+    }
+    logEntries.push(logEntry);
+    console.log(logEntries);
 }
 
 function divide()
 {
     const enteredNumber = getUserInput();
+    const secondNumber = currentResult;
     if (enteredNumber == 0)
     {
         alert ("Cannot divide by zero!");
@@ -34,15 +54,32 @@ function divide()
     const calcDescription = `${currentResult} / ${enteredNumber}`;
     currentResult = currentResult / enteredNumber;
     outputResult(currentResult, calcDescription);
+    const logEntry = {
+        operation: 'DIVIDE',
+        previousResult: secondNumber,
+        newNumber: enteredNumber,
+        result : currentResult,
+    }
+    logEntries.push(logEntry);
+    console.log(logEntries);
     }
 }
 
 function multiply()
 {
     const enteredNumber = getUserInput();
+    const secondNumber = currentResult;
     const calcDescription = `${currentResult} * ${enteredNumber}`;
     currentResult = currentResult * enteredNumber;
     outputResult(currentResult, calcDescription);
+    const logEntry = {
+        operation: 'MULTIPLY',
+        previousResult: secondNumber,
+        newNumber: enteredNumber,
+        result : currentResult,
+    }
+    logEntries.push(logEntry);
+        console.log(logEntries);
 }
 
 addBtn.addEventListener('click', add);
